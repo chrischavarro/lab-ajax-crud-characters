7c8f07bee92a0de1c21bf23ad5a155bfaa82a3d9
Christian Chavarro <christian@clutchprep.com>
2017-11-19 15:05:22 -0500

    Completed exercise

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,83 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList (baseUrl) {
+    $.ajax({
+      method: 'GET',
+      url: baseUrl,
+      success: function(response) {
+        console.log(response)
+        response.forEach((character) => {
+          $('.characters-container').append(`<div class="character-info">
+            <div class="name">Character Name: <b id="fetched-character-name">${character.name}</b></div>
+            <div class="occupation">Character Occupation: <b id="fetched-character-occupation">${character.occupation}</b></div>
+            <div class="debt">Character Debt: <b id="fetched-character-debt">${character.debt}</b></div>
+            <div class="weapon">Character Weapon: <b id="fetched-character-weapon">${character.weapon}</b></div>
+          </div>`)
+        })
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
+  }
+
+  getOneRegister (baseUrl, id) {
+    $.ajax({
+      method: 'GET',
+      url: baseUrl + id,
+      success: function(response) {
+        console.log(response)
+        $('#fetched-character-name').text(response.name)
+        $('#fetched-character-occupation').text(response.occupation)
+        $('#fetched-character-debt').text(response.debt)
+        $('#fetched-character-weapon').text(response.weapon)
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
 
   }
 
-  getOneRegister () {
-
+  createOneRegister (baseUrl, newCharacter) {
+    $.ajax({
+      method: 'POST',
+      url: baseUrl,
+      data: newCharacter,
+      success: function(response) {
+        console.log(response)
+      },
+      error: function(err) {
+        console.log(err)
+      }
+    })
   }
 
-  createOneRegister () {
-
+  updateOneRegister (baseUrl, updatedCharacter, id) {
+    $.ajax({
+      method: 'PATCH',
+      url: baseUrl + id,
+      data: updatedCharacter,
+      success: function(response) {
+        console.log(updatedCharacter)
+      },
+      error: function(err) {
+        console.log('Character not found')
+      }
+    })
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (baseUrl, id) {
+    $.ajax({
+      method: 'DELETE',
+      url: baseUrl + id,
+      success: function(response) {
+        console.log('Character has been successfully deleted')
+      },
+      error: function(err) {
+        console.log('Character not found')
+      }
+    })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,44 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com/characters")
+var URL = 'http://ih-crud-api.herokuapp.com/characters/'
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList(URL)
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    var charId = $('#single-character-id').val()
+    charactersAPI.getOneRegister(URL, charId)
   })
 
   $('#delete-one').on('click', (e) => {
-
+    var charId = $('#delete-character-id').val()
+    charactersAPI.deleteOneRegister(URL, charId)
   })
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault()
+    var charId = $('#edit-character-id').val()
+    var updatedCharacter = {
+      name: $('#edit-character-name').val(),
+      occupation: $('#edit-character-occupation').val(),
+      debt: $('#edit-character-debt').val(),
+      weapon: $('#edit-character-weapon').val()
+    }
 
+    charactersAPI.updateOneRegister(URL, updatedCharacter, charId)
   })
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    var newCharacter = {
+      name: $("#new-character-name").val(),
+      occupation: $("#new-character-occupation").val(),
+      debt: $("#new-character-debt").val(),
+      weapon: $("#new-character-weapon").val()
+    }
+
+    charactersAPI.createOneRegister(URL, newCharacter)
 
   })
 })
